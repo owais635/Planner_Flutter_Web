6e7337bf81f150b3512300d1c86a5a4fb9371e30
Owais <osiddiqui516@gmail.com>
2019-07-19 01:10:09 -0400

    improvements to task

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
+        fontFamily: 'RobotoMono'
       ),
       home: MyWebsite(),
     );
--- lib/widgets/task.dart
+++ lib/widgets/task.dart
@@ -9,32 +9,52 @@ class Task extends StatelessWidget {
   final String date = "Thu., Aug 01";
 
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Column(
-        children: <Widget>[
-          Row(
-                        // mainAxisAlignment: MainAxisAlignment.center,
-
-            children: <Widget>[
-              Text(name),
-              Text(progress.toString()),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text(course),
-              Text(time),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text(type),
-              Text(date),
-            ],
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Row(
+          // mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+                flex: 1,
+                child: 
+                Container(
+                  color: Colors.blue,
+                  // alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 30),
+                )),
+            Expanded(
+                flex: 80,
+                child: Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(name),
+                            Text(progress.toString()),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(course),
+                            Text(time),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(type),
+                            Text(date),
+                          ],
+                        ),
+                      ],
+                    )))
+          ],
+        )
+      ],
     );
   }
 }
