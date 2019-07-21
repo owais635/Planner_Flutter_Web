a34e4f5f059ad85d83d5c0c12556837d113dcbac
Owais <osiddiqui516@gmail.com>
2019-07-21 01:36:58 -0400

    types screen now responsive

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import './screens/tasks_screen.dart';
 import './widgets/task_item.dart';
 import './widgets/app_drawer.dart';
 
-import './screens/landing_page_screen.dart';
+import './screens/type_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'MyApp - Flutter For Web',
       debugShowCheckedModeBanner: true,
       theme: ThemeData(primarySwatch: Colors.deepPurple ,fontFamily: "OpenSans-Regular"  ),
-      home: LandingPageScreen(),
+      home: TypesScreen(),
     );
   }
 }
--- lib/screens/type_screen.dart
+++ lib/screens/type_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_web/material.dart';
+import 'package:my_app/utils/responsiveLayout.dart';
 
 import '../widgets/app_drawer.dart';
 import '../widgets/type_item.dart';
@@ -10,7 +11,22 @@ class TypesScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text('Types'), centerTitle: true),
         drawer: AppDrawer(),
-        body: GridView.count(
+        body: Body());
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+        smallScreen: ListView(
+            children: List.generate(types.length, (index) {
+          return Center(
+              child: TypeItem(
+                  type: types[index],
+                  colour: convertHexColour(typeColourHex[index])));
+        })),
+        largeScreen: GridView.count(
             crossAxisCount: 4,
             children: List.generate(types.length, (index) {
               return Center(
